e15c9c37e2083a2fee92989443e56125b130bbdd
seniorvladislav <vmelikyan161@gmail.com>
2021-11-23 13:18:23 +0300

    Get rid of dotenv on production

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const router = require("express").Router();
 const isEmail = require("validator/lib/isEmail");
 let jwt = require("jsonwebtoken");
